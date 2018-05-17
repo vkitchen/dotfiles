832460c8401715ed005846bdc7c5e38cf75d2cbb
Vaughan Kitchen <v.kitchen@gnoms.org>
2018-05-17 13:32:47 +1200

    Starting a new system. No longer use plan 9 userspace tools

--- config.fish
+++
@@ -1,4 +0,0 @@
-set fish_greeting
-
-set -x PLAN9 /usr/local/plan9
-set -x PATH ~/bin $PLAN9/bin $PATH
--- init.sh
+++ init.sh
@@ -1,47 +1,47 @@
 #!/usr/bin/env bash
 
 packages=(
-  tilix
-  fish
-  thunderbird
   tmux
-  neovim
-  seahorse
-  libXext-devel
-  libX11-devel
-  libXt-devel
-  freetype-devel
-  fontconfig-devel
-  gstreamer1-libav
-  gstreamer1-plugins-bad-nonfree
-  ncmpcpp
+  elinks
+  vim
+  spectrwm
+  Xorg
+  xorg-x11-xinit
+  xorg-x11-drv-intel
+  xorg-x11-drv-libinput
+  firefox
+  telegram-desktop
+  st
+  scrot
+  pulseaudio
+  alsa-utils
+  mpv
   gimp
   inkscape
-  transmission
-  unrar
+  pulseaudio-utils
+  ranger
+  sxiv
+  NetworkManager-tui
+  NetworkManager-wifi
+  wine
+  mutt
+  spotify
+  libreoffice
+  weechat
+  git
+  moreutils
   nmap
-  scrot
+  fish
 )
 
-function enable_rpmfusion {
+function add_repos {
   sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E '%fedora').noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E '%fedora').noarch.rpm
+  sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
+  sudo dnf enable dawid/better_fonts
 }
 
 function install_packages {
   sudo dnf install -y ${packages[*]}
-  # install plan9port
-  pushd /usr/local
-  sudo git clone 'https://github.com/9fans/plan9port'
-  sudo mv plan9port plan9
-  sudo chown -R $USER:$GROUPS plan9
-  pushd /usr/local/plan9
-  sudo ./INSTALL
-  popd
-  popd
-  # use neovim as vim
-  pushd /usr/bin
-  sudo ln -s nvim vim
-  popd
   # set fish as default shell
   sudo usermod -s `which fish` $USER
 }
@@ -51,16 +51,8 @@ function configure_git {
   git config --global user.email "v.kitchen@gnoms.org"
 }
 
-function copy_configs {
-  mkdir -p ~/.config/fish
-  cp config.fish ~/.config/fish/config.fish
-  mkdir -p ~/.config/nvim
-  cp init.vim ~/.config/nvim/init.vim
-}
-
 cp -r bin ~/
 sudo dnf upgrade -y
-enable_rpmfusion
+add_repos
 install_packages
 configure_git
-copy_configs
