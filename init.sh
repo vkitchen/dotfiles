be5accec225e2a812c7596fb3c10d569e66ecd40
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2021-12-14 21:25:00 +1300

    Init script for linking basic configs

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# link to standard home directory type dotfile
+function hlink() {
+	# -e expands links. fails if link is broken
+	if [ -e "$HOME/.$1" -o -L "$HOME/.$1" ]; then
+		echo "rm ~/.$1"
+		rm ~/.$1
+	fi
+	ln -s $PWD/$1 ~/.$1
+}
+
+hlink config/nvim/init.vim
+hlink cwmrc
+hlink kshrc
+hlink profile
+hlink tmux.conf
