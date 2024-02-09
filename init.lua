24e941024bb596754f0c5b919f1ce62068466ab4
Vaughan Kitchen <contact@tophat.nz>
2024-02-09 21:44:59 +1300

    Begin neovim config

---
+++ init.lua
@@ -0,0 +1,13 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+  Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+vim.call('plug#end')
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
