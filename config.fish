96d5f26017381782e5d0ee0507b189b573061c45
Vaughan Kitchen <contact@tophat.nz>
2024-03-27 11:17:57 +1300

    Add java to path

--- config.fish
+++ config.fish
@@ -1,6 +1,11 @@
 set -U fish_greeting
 
+set -x JAVA_HOME /usr/lib/jvm/default-jdk
+
+set -x MANPATH $MANPATH /usr/lib/jvm/default-jdk/man
+
 fish_add_path ~/bin
+fish_add_path /usr/lib/jvm/default-jdk/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
