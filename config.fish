4c3ba25520e66ce02cf86626a88bcae79468b9a1
Vaughan Kitchen <contact@tophat.nz>
2024-03-27 11:18:08 +1300

    Give higher priority ~/bin

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ set -x JAVA_HOME /usr/lib/jvm/default-jdk
 
 set -x MANPATH $MANPATH /usr/lib/jvm/default-jdk/man
 
-fish_add_path ~/bin
+fish_add_path -p ~/bin
 fish_add_path /usr/lib/jvm/default-jdk/bin
 
 if status is-interactive
