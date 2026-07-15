3b10402be4a3607a12f4d1278b01c2a0286bedd4
Vaughan Kitchen <contact@tophat.nz>
2026-07-16 10:11:25 +1200

    Switch to lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,18 +1,5 @@
+require("config.lazy")
+
 local vim = vim
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-  Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-  Plug('lervag/vimtex')
-vim.call('plug#end')
-
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'eex', 'elixir', 'heex' },
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
 
 vim.wo.number = true
---
+++ config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,5 @@
+return {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+}
