06520bd1860c1b09d0ed1f1b7e8959b68ff0b828
Vaughan Kitchen <contact@tophat.nz>
2026-09-09 19:07:45 +1200

    Enable vimtex system calls

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -2,4 +2,14 @@ return {
   "lervag/vimtex",
   lazy = false,     -- we don't want to lazy load VimTeX
   -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        "-pdf",
+        "-shell-escape",
+        "-interaction=nonstopmode",
+        "-synctex=1",
+      },
+    }
+  end
 }
